6f3675239d8a5f9c259ec788b522ccd50b8ba931
rabbit2002 <wonderingstar@163.com>
2020-12-11 20:24:39 +0800

    2020.12.11 chapter09_InheritanceAndPolymorphism Polymorphism   Fix some word chapter10_AbstractClassAndInterface AbstractClass   Fix some word   Add Nested Class   Add some example code chapter10_AbstractClassAndInterface Interface   Add Interface

--- src/JavaOop/chapter10_AbstractClassAndInterface/AbstractClass.java
+++ src/JavaOop/chapter10_AbstractClassAndInterface/AbstractClass.java
@@ -2,30 +2,74 @@ package javaOop.chapter10_AbstractClassAndInterface;
 
 /**
  * 被abstract关键字修饰的Class是Abstract Class
- * 被abstract关键字修饰的Func()是Abstract Func
+ * 被abstract关键字修饰的Func()是Abstract Func()
  * <p>
  * 1.抽象类与普通类一样可以定义实例属性/实例方法/类属性/类方法
- * 2.抽象类独有定义抽象方法(没有方法体),且只有实例方法可以被抽象,实例属性和类方法/类属性均不能抽象
- * 3.如果一个类包含抽象类,则该类只能是抽象类,但抽象类不一定包含抽象方法
+ * 2.抽象类独有抽象方法(没有方法体的方法),且只有实例方法可以被抽象,实例属性和类方法/类属性均不能抽象
+ * 3.如果一个类包含抽象方法,则该类只能是抽象类,但抽象类不一定包含抽象方法
  * 4.只要是抽象类,就不能实例化
- * 5.private关键字不能修饰抽象方法,因为如此做子类将无法实现
+ * 5.private 和 final 关键字不能修饰抽象方法,因为如此做子类将无法实现该方法
  * <p>
- * 抽象类是为了更好的实现多态,抽象类作为父类,子类如果想实例化就一定要实现父类的抽象方法,或者子类自身也是抽象类,
+ * 抽象类是为了更好的实现多态,
+ * 抽象类作为父类,子类继承后如果想实例化,就一定要实现父类的全部抽象方法,或子类自身也是抽象类,
  * 通过抽象方法让子类强制遵守实现(重写)父类的方法,实现多态
  * <p>
  * <p>
- * // TODO 抽象类及接口中的多态 还需校验修改
- * 接口和抽象类比较像定义不完全/没有定义父类方法的状态,所以只会执行子类继承/实现的方法,
- * 也就是用父类指向子类方法,执行子类的实现,
- * 而对于子类自有的方法,只能通过造型后执行
- * <p>
- * <p>
- * 内部类共有四种:成员/静态/局部/匿名
+ * 嵌套类(Nested Class)共有两种:
+ * 1.静态嵌套类(Static Nested Class) 2.非静态嵌套类/内部类(Inner Class)
+ * 其中,内部类又分为:
+ * 1.成员内部类(Member inner class) 2.局部内部类(Local inner class) 3.匿名内部类(Anonymous inner class)
+ * 而包含嵌套类的类,可称为外围类(Enclosing Class)或外部类(Outer Class)
+ * // TODO 需要更详细了解内部类
  *
  * @author RABBIT2002
  * @date 2020/12/10
  */
 public abstract class AbstractClass {
+    int anIntA = 10;
+    static int anIntStaticA = 20;
+
+    void func1() {
+        System.out.println("1");
+    }
+
+    abstract void func2();
+}
+
+class Son extends AbstractClass {
+    int anIntB = 10;
+    static int anIntStaticB = 20;
+
+    @Override
+    void func2() {
+        System.out.println("2");
+    }
+}
+
+class Test {
     public static void main(String[] args) {
+        Son son = new Son();
+        son.func1();
+        son.func2();
+        System.out.println(son.anIntA);
+        System.out.println(son.anIntB);
+        System.out.println(AbstractClass.anIntStaticA);
+        System.out.println(Son.anIntStaticB);
+
+        AbstractClass abstractClass = new AbstractClass() {
+            // 内部匿名类 Anonymous inner class
+
+            int anIntC = 10;
+            // static int anIntStaticC = 20;
+
+            @Override
+            void func2() {
+                System.out.println("2");
+            }
+        };
+        abstractClass.func1();
+        abstractClass.func2();
+        System.out.println(abstractClass.anIntA);
+        System.out.println(AbstractClass.anIntStaticA);
     }
 }
